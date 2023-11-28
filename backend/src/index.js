96c1f28b0e6ff4cfdb061cd63d17fb53b7d41fe4
[yousincha] <[yousincha@naver.com]>
2023-11-28 11:35:32 +0900

    20231128-5 commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,34 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+const cors = require("cors");
 const port = 4000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("연결 완료");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.get("/", (req, res) => {
-  res.send("안녕하세요111");
+  res.send("안녕하세요");
 });
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.use(express.static(path.join(__dirname, "../uploads")));
 
 app.listen(port, () => {
   console.log(`${port}번에서 실행이 되었습니다.`);
